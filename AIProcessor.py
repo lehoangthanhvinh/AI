b37f5fa5edb043344b7bc597aef3e7ac53d7efc9
unknown <lethanhvinh3011@gmail.com>
2025-05-29 16:27:36 +0700

    290525 AI commit

--- AIProcessor.py
+++ AIProcessor.py
@@ -19,78 +19,95 @@ def AIMove(FENCode):
     global MemList
     (PlayingBoard,MoveCount,TurnCount)=FENToBS(FENCode)
     PosMove=PosibleMove(PlayingBoard)
+    FENPart=FENCode.split(' ',6)
+    FENTrim=' '.join(FENPart[:4])    
     #remove later
     MemList={}
     #------------
-    if PlayingBoard.turn==0:Maximize(PlayingBoard,0,int(MoveCount),int(TurnCount),-math.inf)
-    else: Minimize(PlayingBoard,0,int(MoveCount),int(TurnCount),math.inf)
-    FENPart=FENCode.split(' ',6)
-    FENTrim=' '.join(FENPart[:4])
-    for elem in MemList:print(elem)
     try:
         return MemList[FENTrim][0]
     except:
+        Evalue=EvaluateBoard(PlayingBoard,int(TurnCount))
+    if PlayingBoard.turn==0:Maximize(PlayingBoard,0,Evalue,FENTrim,int(TurnCount),-math.inf,math.inf)
+    else: Minimize(PlayingBoard,0,Evalue,FENTrim,int(TurnCount),-math.inf,math.inf)
+    #for elem in MemList:print(MemList[elem])
+    if MemList[FENTrim][0]!=((8,8),(8,8)):
+        return MemList[FENTrim][0]
+    else:
         print("Random Move")
         return random.choice(PosMove)
-def Maximize(PlayingBoard,SearchDepth,MoveCount,TurnCount,CurScore):
-    EvalFEN=BSToFEN(PlayingBoard,MoveCount,TurnCount)
-    FENPart=EvalFEN.split(' ',6)
-    FENTrim=' '.join(FENPart[:4])
+def Maximize(PlayingBoard,SearchDepth,EvalueScore,FENTrim,TurnCount,alpha,beta):
     try: 
         return MemList[FENTrim][1]
     except:
         pass
-    Score=EvaluateBoard(PlayingBoard,TurnCount)
-    if Score>=100000 or Score<=-100000 or SearchDepth==depth:
-        return Score
     PosMove=PosibleMove(PlayingBoard)
     if not PosMove:
-        return -100000
-    Score=-math.inf
+        return -10000
+    if SearchDepth==depth:
+        MemList[FENTrim]=(random.choice(PosMove),EvalueScore)
+        return EvalueScore
+    Response=(((8,8),(8,8)),alpha)
     for Move in PosMove:
         MoveMade=MakeMove(PlayingBoard,Move[0],Move[1])
-        EvalScore=Minimize(PlayingBoard,SearchDepth+1,MoveCount,TurnCount,Score)
+        Evalue=EvaluateBoard(PlayingBoard,TurnCount)
+        Str='  '*SearchDepth
+        #print(f"{Str}Move from {Move[0]} to {Move[1]} Score={Evalue}")
+        if Evalue<-10000 or Evalue>10000 or Evalue<EvalueScore-100:
+            UnmakeMove(PlayingBoard,Move[1],Move[0],MoveMade)
+            continue
+        #print(f"{Str}Keep going")
+        NextFENTrim=BSToFEN(PlayingBoard,0,TurnCount)
+        NextFENPart=NextFENTrim.split(' ',6)
+        NextFENTrim=' '.join(NextFENPart[:4])
+        Score=Minimize(PlayingBoard,SearchDepth+1,Evalue,NextFENTrim,TurnCount,Response[1],beta)
         UnmakeMove(PlayingBoard,Move[1],Move[0],MoveMade)
-        #print(f"Move from {Col[Move[0][1]]+Row[Move[0][0]]} to {Col[Move[1][1]]+Row[Move[1][0]]}, Score={EvalScore}")
-        if Score<EvalScore:
-            Score=EvalScore
-            MemList[FENTrim]=(Move,Score)
-            if Score<CurScore:
-                return Score
-    return Score
-def Minimize(PlayingBoard,SearchDepth,MoveCount,TurnCount,CurScore):
-    EvalFEN=BSToFEN(PlayingBoard,MoveCount,TurnCount)
-    FENPart=EvalFEN.split(' ',6)
-    FENTrim=' '.join(FENPart[:4])
+        if Score>Response[1]:
+            Response=(Move,Score)
+            if Response[1]>=beta: break
+    MemList[FENTrim]=Response
+    return Response[1]
+def Minimize(PlayingBoard,SearchDepth,EvalueScore,FENTrim,TurnCount,alpha,beta):
     try: 
         return MemList[FENTrim][1]
     except:
         pass
-    Score=EvaluateBoard(PlayingBoard,TurnCount)
-    if Score>=100000 or Score<=-100000 or SearchDepth==depth:
-        return Score
     PosMove=PosibleMove(PlayingBoard)
     if not PosMove:
-        return 100000
-    Score=math.inf
+        return 10000
+    if SearchDepth==depth:
+        MemList[FENTrim]=(random.choice(PosMove),EvalueScore)
+        return EvalueScore
+    Response=(((8,8),(8,8)),beta)
     for Move in PosMove:
         MoveMade=MakeMove(PlayingBoard,Move[0],Move[1])
-        EvalScore=Maximize(PlayingBoard,SearchDepth+1,MoveCount,TurnCount+1,Score)
+        Evalue=EvaluateBoard(PlayingBoard,TurnCount)
+        Str='  '*SearchDepth
+        #print(f"{Str}Move from {Move[0]} to {Move[1]} Score={Evalue}")
+        if Evalue<-10000 or Evalue>10000 or Evalue>EvalueScore+100:
+            UnmakeMove(PlayingBoard,Move[1],Move[0],MoveMade)
+            continue
+        #print(f"{Str}Keep going")
+        NextFENTrim=BSToFEN(PlayingBoard,1,TurnCount)
+        NextFENPart=NextFENTrim.split(' ',6)
+        NextFENTrim=' '.join(NextFENPart[:4])
+        Score=Maximize(PlayingBoard,SearchDepth+1,Evalue,NextFENTrim,TurnCount+1,alpha,Response[1])
         UnmakeMove(PlayingBoard,Move[1],Move[0],MoveMade)
-        #print(f"Move from {Col[Move[0][1]]+Row[Move[0][0]]} to {Col[Move[1][1]]+Row[Move[1][0]]}, Score={EvalScore}")
-        if Score>EvalScore:
-            Score=EvalScore
-            MemList[FENTrim]=(Move,Score)
-            if Score>CurScore:
-                return Score
-    return Score
+        if Score<Response[1]:
+            Response=(Move,Score)
+            if Response[1]<=alpha: break
+    MemList[FENTrim]=Response
+    return Response[1]
 def EvaluateBoard(PlayingBoard,TurnCount):
     Board=PlayingBoard.board
     Score=0
     for i in range(8):
         for j in range(8):
             if Board[i][j]=='.':continue
-            PositionMap=PositionValue[min((9+TurnCount)//25,2)][Board[i][j]]
+            if TurnCount<=15:TurnCount=0
+            elif TurnCount<=40:TurnCount=1
+            else:TurnCount=2
+            PositionMap=PositionValue[TurnCount][Board[i][j]]
             if Board[i][j] in Piece[1]:
                 Score=Score+PieceValue[Board[i][j]]-PositionMap[7-i][j]
             else: 
